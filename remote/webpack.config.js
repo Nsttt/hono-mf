ebe1f9a58e19174934b7cc655c2d7faf19bba5ad
Néstor <nestor@nstlopez.com>
2025-09-04 13:37:21 +0200

    trying stuff

--- remote/webpack.config.js
+++ remote/webpack.config.js
@@ -1,51 +1,56 @@
-const { ModuleFederationPlugin } = require('@module-federation/enhanced/webpack');
-const path = require('path');
+const {
+  ModuleFederationPlugin,
+} = require("@module-federation/enhanced/webpack");
+const path = require("path");
 
 module.exports = {
-  target: 'async-node',
-  mode: 'production',
-  entry: './src/index.ts',
+  target: "async-node",
+  mode: "production",
+  entry: "./src/index.ts",
+  devtool: false,
   resolve: {
-    extensions: ['.ts', '.js'],
+    extensions: [".ts", ".js"],
   },
   module: {
     rules: [
       {
         test: /\.ts$/,
         use: {
-          loader: 'ts-loader',
+          loader: "ts-loader",
           options: {
-            configFile: 'tsconfig.json',
+            configFile: "tsconfig.json",
           },
         },
         exclude: /node_modules/,
       },
       {
         test: /\.js$/,
-        type: 'javascript/auto',
+        type: "javascript/auto",
       },
     ],
   },
   plugins: [
     new ModuleFederationPlugin({
-      name: 'remote1',
-      filename: 'remoteEntry.js',
+      name: "remote1",
+      filename: "remoteEntry.js",
       exposes: {
-        './utils': './src/utils.ts',
-        './api': './src/api.ts',
-        './app': './src/index.ts',
+        "./utils": "./src/utils.ts",
+        "./api": "./src/api.ts",
+        "./app": "./src/index.ts",
       },
-      library: { type: 'commonjs-module' },
-      runtimePlugins: [require.resolve('@module-federation/node/runtimePlugin')],
-      remoteType: 'script',
+      library: { type: "commonjs-module" },
+      runtimePlugins: [
+        require.resolve("@module-federation/node/runtimePlugin"),
+      ],
+      remoteType: "script",
       dts: false, // Disable type generation for now
     }),
   ],
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: '[name].js',
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name].js",
     clean: true,
-    publicPath: 'auto',
+    publicPath: "auto",
   },
   // externals: {
   //   'hono': 'commonjs hono',
